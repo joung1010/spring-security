0cbfb84b9d359d61428ea7207797dbf93490c25c
mason park <joung4342@gmail.com>
2024-07-15 20:18:32 +0900

    feat(security): 사용자 정의 구현(자바 설정파일)

--- src/main/java/com/business/security/common/config/SecurityConfigBasic.java
+++ src/main/java/com/business/security/common/config/SecurityConfigBasic.java
@@ -6,6 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -30,5 +34,26 @@ public class SecurityConfigBasic {
 
         log.info("Basic Security Filter Chain Setting End =============");
         return http.build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user")
+                .password("{noop}1111")
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User.withUsername("user2")
+                .password("{noop}1111")
+                .roles("USER")
+                .build();
+
+
+        UserDetails user3 = User.withUsername("user3")
+                .password("{noop}1111")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user,user2,user3);
     }
 }

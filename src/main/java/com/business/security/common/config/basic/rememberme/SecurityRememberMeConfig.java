67c5395fee4b0d523d6bb7129a158c6fbb7299f0
mason park <joung4342@gmail.com>
2024-08-13 20:13:57 +0900

    feat(security): RememberMe 구현

--- src/main/java/com/business/security/common/config/basic/rememberme/SecurityRememberMeConfig.java
+++ src/main/java/com/business/security/common/config/basic/rememberme/SecurityRememberMeConfig.java
@@ -1,6 +1,6 @@
-/*
-package com.business.security.common.config.basic.httpBasic;
+package com.business.security.common.config.basic.rememberme;
 
+import com.business.security.common.config.basic.httpBasic.CustomAuthenticationEntryPoint;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,27 +13,34 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-*/
 /**
- * <b> SecurityHttpBasicConfig </b>
+ * <b> SecurityRememberMeConfig </b>
  *
  * @author jh.park
  * @version 0.1.0
- * @since 2024-08-06
- *//*
+ * @since 2024-08-13
+ */
 
 @Slf4j
 @EnableWebSecurity
 @Configuration
-public class SecurityHttpBasicConfig {
+public class SecurityRememberMeConfig {
     public static final String SECURITY_FILTER_CHAIN_NAME = "security-filter-chain";
 
     @Bean(SECURITY_FILTER_CHAIN_NAME)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-//                .httpBasic(Customizer.withDefaults())
-                .httpBasic(basic -> basic.authenticationEntryPoint(new CustomAuthenticationEntryPoint()))
+                .formLogin(Customizer.withDefaults())
+                .rememberMe(rememberMeConfigurer ->
+                                rememberMeConfigurer
+//                                        .alwaysRemember(true) // 항상 기억하기 인증을 활성화 시킴(체크박스 없이도)
+                                        .tokenValiditySeconds(3600) // 토큰 유지시간 (밀리세컨드)
+                                        .userDetailsService(userDetailsService())
+                                        .rememberMeParameter("remember")
+                                        .rememberMeCookieName("remember")
+                                        .key("myKey-security")
+                        )
         ;
 
         return http.build();
@@ -48,5 +55,5 @@ public class SecurityHttpBasicConfig {
 
         return new InMemoryUserDetailsManager(user);
     }
+
 }
-*/

f3977bb4f974c80304eb1a6343bf3859250a2985
mason park <joung4342@gmail.com>
2024-09-03 20:56:17 +0900

    feat(security): Authentication 로직 흐름

--- src/main/java/com/business/security/common/config/basic/authentication/architecture/SecurityAuthenticationConfig.java
+++ src/main/java/com/business/security/common/config/basic/authentication/architecture/SecurityAuthenticationConfig.java
@@ -1,5 +1,4 @@
-/*
-package com.business.security.common.config.basic.authentication.process.requestcache;
+package com.business.security.common.config.basic.authentication.architecture;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,57 +10,39 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 import org.springframework.security.web.savedrequest.SavedRequest;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.io.IOException;
 
-*/
 /**
- * <b> SecurityAnonymousConfig </b>
+ * <b> SecurityAuthenticationConfig </b>
  *
  * @author jh.park
  * @version 0.1.0
- * @since 2024-08-20
- *//*
-
-
+ * @since 2024-09-03
+ */
 @Slf4j
 @EnableWebSecurity
 @Configuration
-public class SecurityCacheConfig {
-
+public class SecurityAuthenticationConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
         HttpSessionRequestCache requestCache = new HttpSessionRequestCache();
         requestCache.setMatchingRequestParameterName("customParam=y");
 
         http.authorizeRequests(auth -> auth
                         .requestMatchers("logoutSuccess").permitAll()
-                        .anyRequest().authenticated())
-                .formLogin(form -> form.successHandler(new AuthenticationSuccessHandler() {
-                    @Override
-                    public void onAuthenticationSuccess(HttpServletRequest request,
-                                                        HttpServletResponse response,
-                                                        Authentication authentication) throws IOException, ServletException {
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
 
-                        SavedRequest savedRequest = requestCache.getRequest(request, response);
-                        String redirectUrl = savedRequest.getRedirectUrl();
-                        response.sendRedirect(redirectUrl);
-                    }
-                }))
-                .requestCache(cache -> cache.requestCache(requestCache))
         ;
 
 
@@ -78,4 +59,3 @@ public class SecurityCacheConfig {
         return new InMemoryUserDetailsManager(user);
     }
 }
-*/

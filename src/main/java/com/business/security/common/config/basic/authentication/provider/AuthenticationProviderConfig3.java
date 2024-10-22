18829f1675a8079e6b8efda002bc6a88a3463f67
mason park <joung4342@gmail.com>
2024-10-22 21:08:15 +0900

    feat(security): AuthenticationProvider 소스4

--- src/main/java/com/business/security/common/config/basic/authentication/provider/AuthenticationProviderConfig3.java
+++ src/main/java/com/business/security/common/config/basic/authentication/provider/AuthenticationProviderConfig3.java
@@ -1,15 +1,10 @@
-/*
 package com.business.security.common.config.basic.authentication.provider;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -18,41 +13,32 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-*/
+
 /**
  * <b> AuthenticationProviderConfig </b>
  *
  * @author jh.park
  * @version 0.1.0
  * @since 2024-10-22
- *//*
+ */
 
 
 @Slf4j
 @EnableWebSecurity
 @Configuration
-public class AuthenticationProviderConfig2 {
+public class AuthenticationProviderConfig3 {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http
-            , AuthenticationManagerBuilder builder
-            , AuthenticationConfiguration configuration ) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        // 기존 HttpSecurity가 가지고있는 AuthenticationMangerBuilder를 가져옴
-        AuthenticationManagerBuilder managerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        // providers에 우리가 만든 커스텀 provider를 추가
-        managerBuilder.authenticationProvider(customAuthenticationProvider());
-        // 기존 parent에 설정되어있던 DaoAuthenticationProvider를 원복
-        ProviderManager providerManager = (ProviderManager)configuration.getAuthenticationManager();
-        providerManager.getProviders().remove(0); // 우리가 만든 Provider를 제거
 
-        builder.authenticationProvider(new DaoAuthenticationProvider());//자동설정 Provider 추가
 
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/").permitAll()
                         .anyRequest().authenticated())
-        ;
+                .authenticationProvider(customAuthenticationProvider())
+                .authenticationProvider(customAuthenticationProvider2())
         ;
         return http.build();
     }
@@ -64,10 +50,16 @@ public class AuthenticationProviderConfig2 {
     }
 
     @Bean
+    public AuthenticationProvider customAuthenticationProvider2() {
+        //parent 에 추가
+        return new CustomAuthenticationProvider2();
+    }
+
+    @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user = User.withUsername("user").password("{noop}1111").roles("USER").build();
         return new InMemoryUserDetailsManager(user);
     }
 
 }
-*/
+

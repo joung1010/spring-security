823d056133e23b0a5cad52333e2321a31db10a52
mason park <joung4342@gmail.com>
2024-10-22 20:57:01 +0900

    feat(security): AuthenticationProvider 소스2(기본적인 추가)

--- src/main/java/com/business/security/common/config/basic/authentication/provider/AuthenticationProviderConfig2.java
+++ src/main/java/com/business/security/common/config/basic/authentication/provider/AuthenticationProviderConfig2.java
@@ -1,9 +1,9 @@
-/*
 package com.business.security.common.config.basic.authentication.provider;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,38 +13,37 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-*/
 /**
  * <b> AuthenticationProviderConfig </b>
  *
  * @author jh.park
  * @version 0.1.0
  * @since 2024-10-22
- *//*
-
+ */
 
 @Slf4j
 @EnableWebSecurity
 @Configuration
-public class AuthenticationProviderConfig {
+public class AuthenticationProviderConfig2 {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        // 적용 1번
-        AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        builder.authenticationProvider(new CustomAuthenticationProvider());
-        builder.authenticationProvider(new CustomAuthenticationProvider2());
+
 
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/").permitAll()
                         .anyRequest().authenticated())
-//                .authenticationProvider(new CustomAuthenticationProvider()) //방식2번
-//                .authenticationProvider(new CustomAuthenticationProvider2())
         ;
         ;
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        //parent 에 추가
+        return new CustomAuthenticationProvider();
     }
 
     @Bean
@@ -54,4 +53,3 @@ public class AuthenticationProviderConfig {
     }
 
 }
-*/

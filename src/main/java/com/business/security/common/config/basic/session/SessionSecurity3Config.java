2478919abfb88fff3b2bb7fadf0b14ea000a3d6c
mason park <joung4342@gmail.com>
2025-01-21 21:23:35 +0900

    feat(security): 세션 인증 정책

--- src/main/java/com/business/security/common/config/basic/session/SessionSecurity3Config.java
+++ src/main/java/com/business/security/common/config/basic/session/SessionSecurity3Config.java
@@ -1,4 +1,3 @@
-/*
 package com.business.security.common.config.basic.session;
 
 import lombok.extern.slf4j.Slf4j;
@@ -6,36 +5,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-*/
 /**
  * <b> SessionSecurityConfig </b>
  *
  * @author jh.park
  * @version 0.1.0
  * @since 2025-01-20
- *//*
-
+ */
 @Slf4j
 @Configuration
-public class SessionSecurity2Config {
+public class SessionSecurity3Config {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.authorizeRequests(auth -> auth
-                        .requestMatchers("/login").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(Customizer.withDefaults())
                 .sessionManagement(session ->
-                        session.sessionFixation(fix ->
-                                fix.changeSessionId() //고정 보호X
-                        )
+                      session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
         ;
 
@@ -48,4 +43,3 @@ public class SessionSecurity2Config {
         return new InMemoryUserDetailsManager(user);
     }
 }
-*/

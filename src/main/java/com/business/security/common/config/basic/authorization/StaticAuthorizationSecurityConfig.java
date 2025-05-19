7d58d8717a19eb6ab9d828d2d34416f14d46ca8e
mason park <joung4342@gmail.com>
2025-05-19 20:49:22 +0900

    feat(security): 정적자원 관리1

---
+++ src/main/java/com/business/security/common/config/basic/authorization/StaticAuthorizationSecurityConfig.java
@@ -0,0 +1,48 @@
+package com.business.security.common.config.basic.authorization;
+
+import lombok.extern.slf4j.Slf4j;import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.ApplicationContext;import org.springframework.context.annotation.Bean;import org.springframework.context.annotation.Configuration;import org.springframework.security.config.Customizer;import org.springframework.security.config.annotation.web.builders.HttpSecurity;import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;import org.springframework.security.core.userdetails.User;import org.springframework.security.core.userdetails.UserDetails;import org.springframework.security.core.userdetails.UserDetailsService;import org.springframework.security.provisioning.InMemoryUserDetailsManager;import org.springframework.security.web.SecurityFilterChain; /**
+ * <b> StaticAuthorizationSecurityConfig </b>
+ *
+ * @author jh.park
+ * @version 0.1.0
+ * @since 2025-05-19
+ */
+
+@Slf4j
+@EnableWebSecurity
+@ConditionalOnProperty(value = "security.type", havingValue = "static-authorization-1", matchIfMissing = false)
+
+@Configuration
+public class StaticAuthorizationSecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> {
+            web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+        };
+    }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, ApplicationContext context) throws Exception {
+
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll())
+                .formLogin(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user").password("{noop}1111").roles("USER").build();
+        UserDetails manager = User.withUsername("db").password("{noop}1111").roles("DB").build();
+        UserDetails admin = User.withUsername("admin").password("{noop}1111").roles("ADMIN","SECURE").build();
+        return new InMemoryUserDetailsManager(user, manager, admin);
+    }
+}
